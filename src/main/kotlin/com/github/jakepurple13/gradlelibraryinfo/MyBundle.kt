736f2394c342929e27869127ddb4112a98a43185
GitHub Action <action@github.com>
2021-07-16 15:48:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/jakepurple13/gradlelibraryinfo/MyBundle.kt
+++ src/main/kotlin/com/github/jakepurple13/gradlelibraryinfo/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jakepurple13.gradlelibraryinfo
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/jakepurple13/gradlelibraryinfo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jakepurple13.gradlelibraryinfo.services
+
+import com.github.jakepurple13.gradlelibraryinfo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
